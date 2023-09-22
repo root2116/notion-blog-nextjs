3c700959c54b4d63844bb0ca10f7a2c3b929fee7
root2116 <root6889@icloud.com>
2023-09-22 11:36:18 +0900

    add sitemap.xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,17 +1,17 @@
 
-const Sitemap = () => { };
+// const Sitemap = () => { };
 
-export const getServerSideProps = async ({ res }) => {
-    const response = await fetch("https://just-an-asile.vercel.app/api/generate-sitemap");
-    const text = await response.text();
+// export const getServerSideProps = async ({ res }) => {
+//     const response = await fetch("https://just-an-asile.vercel.app/api/generate-sitemap");
+//     const text = await response.text();
 
-    res.setHeader("Content-Type", "text/xml");
-    res.write(text);
-    res.end();
+//     res.setHeader("Content-Type", "text/xml");
+//     res.write(text);
+//     res.end();
 
-    return {
-        props: {},
-    };
-};
+//     return {
+//         props: {},
+//     };
+// };
 
-export default Sitemap;
+// export default Sitemap;
